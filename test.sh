1e043847b9420896cb6fada93e3c1fd4286b6d23
Julien Dufresne <git@dfrsn.me>
2018-09-22 23:24:52 +0200

    Use alpine images

--- test.sh
+++ test.sh
@@ -7,18 +7,41 @@ set -euo pipefail
 # Avoid using space as a separator (default IFS=$' \t\n')
 IFS=$'\n\t'
 
-
 DOCKER_ORGANIZATION=$1
 DOCKER_IMAGE=$2
 DOCKER_IMAGE_TAG=$3
+DOCKER_BUILD_ARGS=()
 
 FMT="\033[38;5;106m%s\033[39m\n"
 ERR="\033[38;5;208m%s\033[39m\n"
 
-if [[ -v TRAVIS ]]; then
-    (set -x; docker build --cache-from composer:latest --tag "$DOCKER_ORGANIZATION/$DOCKER_IMAGE:$DOCKER_IMAGE_TAG" --cache-from "$DOCKER_ORGANIZATION/$DOCKER_IMAGE:$DOCKER_IMAGE_TAG" $DOCKER_IMAGE/$DOCKER_IMAGE_TAG)
+if ! [[ -d "$DOCKER_IMAGE/$DOCKER_IMAGE_TAG" ]]; then
+    >&2 printf ${ERR} "Dir not found: $DOCKER_IMAGE/$DOCKER_IMAGE_TAG"
+    exit 1
+fi
+
+cd "$DOCKER_IMAGE/$DOCKER_IMAGE_TAG"
+
+if [[ -v PHP_VERSION ]]; then
+    DOCKER_IMAGE_TAG="$DOCKER_IMAGE_TAG"-php"$PHP_VERSION"
+    DOCKER_BUILD_ARGS+=("--build-arg" "PHP_VERSION=$PHP_VERSION")
+fi
+
+DOCKER_BUILD_ARGS+=("--tag" "$DOCKER_ORGANIZATION/$DOCKER_IMAGE:$DOCKER_IMAGE_TAG")
+if ! [[ -v TRAVIS ]]; then
+    (
+        set -x;
+        docker build ${DOCKER_BUILD_ARGS[@]} .
+    )
 else
-    (set -x; docker build --cache-from composer:latest --tag "$DOCKER_ORGANIZATION/$DOCKER_IMAGE:$DOCKER_IMAGE_TAG" $DOCKER_IMAGE/$DOCKER_IMAGE_TAG)
+    (
+        set -x;
+        docker build \
+                 ${DOCKER_BUILD_ARGS[@]} \
+                 --cache-from "$DOCKER_ORGANIZATION/$DOCKER_IMAGE:$DOCKER_IMAGE_TAG" \
+                 --cache-from composer:latest \
+                 .
+    )
 fi
 
 printf ${FMT} "docker images"
@@ -45,6 +68,7 @@ if [[ "$DOCKER_IMAGE" == "php" ]]; then
         exit 1
     fi
     [[ -v TRAVIS ]] || rm test-as-user
+    printf ${FMT} "OK"
 
     printf ${FMT} "Write file as root user"
     docker run --rm --volume $PWD:/app $DOCKER_ORGANIZATION/$DOCKER_IMAGE:$DOCKER_IMAGE_TAG php -r 'shell_exec("echo a > test-as-root");'
@@ -57,14 +81,23 @@ if [[ "$DOCKER_IMAGE" == "php" ]]; then
         exit 1
     fi
     [[ -v TRAVIS ]] || sudo rm test-as-root
+    printf ${FMT} "OK"
 
-    printf ${FMT} "test disabling XDEBUG"
-
-    if docker run --rm -e XDEBUG_ENABLED=0 --volume $PWD:/app $DOCKER_ORGANIZATION/$DOCKER_IMAGE:$DOCKER_IMAGE_TAG php -m | grep -q "xdebug"
+    printf ${FMT} "test enabling XDEBUG as current user"
+    if ! docker run --rm -e XDEBUG_ENABLED=1 --volume $PWD:/app --user=$(id -u):$(id -g) $DOCKER_ORGANIZATION/$DOCKER_IMAGE:$DOCKER_IMAGE_TAG php -m | grep -q "xdebug"
     then
-        >&2 printf ${ERR} "Unable to deactivate XDEBUG"
+        >&2 printf ${ERR} "Unable to activate XDEBUG"
         exit 1
     fi
+    printf ${FMT} "OK"
+
+    printf ${FMT} "test enabling XDEBUG"
+    if ! docker run --rm -e XDEBUG_ENABLED=1 --volume $PWD:/app $DOCKER_ORGANIZATION/$DOCKER_IMAGE:$DOCKER_IMAGE_TAG php -m | grep -q "xdebug"
+    then
+        >&2 printf ${ERR} "Unable to activate XDEBUG"
+        exit 1
+    fi
+    printf ${FMT} "OK"
 fi
 
 exit 0
